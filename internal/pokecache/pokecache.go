a88a1617940ee2bb3abf1ce4a8610cf3abdc13b1
Will Keeble <will.a.keeble@gmail.com>
2024-12-02 00:46:31 +1100

    setup cache

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,66 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mu    *sync.Mutex
+}
+
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mu:    &sync.Mutex{},
+	}
+
+	c.reapLoop(interval)
+
+	return c
+}
+
+func (c Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       val,
+	}
+}
+
+func (c Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	val, exists := c.cache[key]
+	if !exists {
+		return nil, false
+	}
+
+	return val.val, true
+}
+
+func (c Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, k)
+		}
+	}
+}
